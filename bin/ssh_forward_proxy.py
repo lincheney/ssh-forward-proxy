7bd6ab271cf210690fbedb02b4f11ec1a3e3e90f
Cheney Lin <lincheney@gmail.com>
2015-02-28 23:00:57 +1100

    make the proxy act as a pipe instead of a server

--- bin/ssh_forward_proxy.py
+++ bin/ssh_forward_proxy.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import select
-import select
 import paramiko
 import threading
 
@@ -43,93 +42,49 @@ class FakeSocket:
         sys.stdin.close()
         sys.stdout.close()
 
-class Proxy(threading.Thread):
-    daemon = True
+class Pipe:
+    def run_command(self, client_channel, command):
+        client_channel.send('Hello %s!\n' % self.username)
+        client_channel.send('Command: %s\n' % command)
 
-    def __init__(self, server):
-        threading.Thread.__init__(self)
-        self.server = server
-        self.transport = self.server.transport
+    def run(self, username, client_channel, command):
+        self.username = username
+        self.run_command(client_channel, command)
+        client_channel.close()
 
-    def _run(self, client_channel):
-        username = self.server.user_queue.get()
-        client_channel.send('Hello %s!' % username)
-
-        command = self.server.exec_queue.get()
-        client_channel.send('Command: %s' % command)
-
-    def run(self):
-        try:
-            client_channel = self.transport.accept(20)
-            assert(client_channel is not None)
-        except Exception as e:
-            logging.exception('Failed to open channel to client')
-            return
-
-        try:
-            self._run(client_channel)
-        except Exception as e:
-            logging.exception('Error serving')
-        finally:
-            client_channel.close()
-
-class ConnectionServer(paramiko.ServerInterface):
+class Proxy(paramiko.ServerInterface):
+    timeout = 10
     host_key = paramiko.RSAKey(filename='server-key')
 
-    def __init__(self, conn, addr):
-        self.exec_queue = queue.Queue()
-        self.user_queue = queue.Queue()
+    def __init__(self):
+        self.username = None
+        self.queue = queue.Queue()
 
-        self.transport = paramiko.Transport(conn)
+        self.transport = paramiko.Transport(FakeSocket())
         self.transport.add_server_key(self.host_key)
         self.transport.start_server(server=self)
 
-        self.thread = Proxy(self)
-        self.thread.start()
+        try:
+            channel, command = self.queue.get(self.timeout)
+        except queue.Empty:
+            logging.error('Client passed no commands')
+            sys.exit(1)
+        Pipe().run(self.username, channel, command)
 
     def check_channel_request(self, kind, chanid):
         return paramiko.OPEN_SUCCEEDED
-    def check_auth_password(self, username, password):
-        self.user_queue.put(username)
-        return paramiko.AUTH_SUCCESSFUL
-    def check_auth_publickey(self, username, key):
-        self.user_queue.put(username)
-        return paramiko.AUTH_SUCCESSFUL
+
     def check_auth_none(self, username):
-        self.user_queue.put(username)
+        self.username = username
         return paramiko.AUTH_SUCCESSFUL
     
     def get_allowed_auths(self, username):
-        return 'password,publickey'
+        return 'none'
 
-    def check_channel_shell_request(self, channel):
-        return True
     def check_channel_exec_request(self, channel, command):
-        self.exec_queue.put(command)
+        self.queue.put((channel, command))
         return True
-    def check_channel_env_request(self, channel, name, value):
-        return False
-
-def run_server(host, port=SSH_PORT):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    logging.info('bind()')
-    sock.bind((host, port))
-
-    logging.info('listen()')
-    sock.listen(10)
-
-    try:
-        while True:
-            r, w, x = select.select([sock], [], [], 1)
-            if sock in r:
-                conn, addr = sock.accept()
-                logging.info('Got a connection!')
-                ConnectionServer(conn, addr)
-    except KeyboardInterrupt:
-        sock.close()
-        sys.exit(0)
 
 if __name__ == '__main__':
-    run_server('', SSH_PORT)
+    Proxy()
 
