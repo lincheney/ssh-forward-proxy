ddb3e24bde1de93a638cd96cd77d72788cfc8ae9
Cheney Lin <lincheney@gmail.com>
2015-04-19 17:19:14 +1000

    overhaul; merge the server into the proxy

--- bin/ssh_forward_proxy.py
+++ bin/ssh_forward_proxy.py
@@ -13,100 +13,82 @@ except ImportError:
 import logging
 import argparse
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
-def make_client(host, port, username):
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-    logging.info('Connecting to ssh host %s@%s:%d ...', username, host, port)
-    client.connect(host, port, username=username)
-    return client
-
-class FakeSocket:
-    """
-    Fake socket to read from stdin and write to stdout
-    conforming to the interface specified at
-    http://docs.paramiko.org/en/1.15/api/transport.html
-    """
-
-    timeout = None
-    def settimeout(self, timeout):
-        self.timeout = timeout
-
-    def send(self, string):
-        if sys.stdout.closed:
-            return 0
-        return os.write(sys.stdout.fileno(), string)
-
-    def recv(self, count):
-        if sys.stdin.closed:
-            return ''
-        r, w, x = select.select([sys.stdin], [], [], self.timeout)
-        if sys.stdin in r:
-            return os.read(sys.stdin.fileno(), count)
-        raise socket.timeout()
-
-    def close(self):
-        sys.stdin.close()
-        sys.stdout.close()
-
-class Pipe:
-    def __init__(self, client, remote):
-        self.client = client
-        self.remote = remote
-
-    def run(self, command):
-        self.remote.exec_command(command)
-
-        size = 1024
-        while True:
-            r, w, x = select.select([self.client, self.remote], [], [])
-
-            if self.remote in r:
-                stdout = self.remote.recv(size)
-                if stdout:
-                    self.client.sendall(stdout)
-                stderr = self.remote.recv_stderr(size)
-                if stderr:
-                    self.client.sendall_stderr(stderr)
-                if not stdout and not stderr:
-                    logging.debug('Output streams closed')
-                    break
-
-            if self.client in r:
-                content = self.client.recv(size)
-                if not content:
-                    logging.debug('Input stream closed')
-                    break
-                self.remote.sendall(content)
-
-        if self.remote.exit_status_ready():
-            status = self.remote.recv_exit_status()
-            self.client.send_exit_status(status)
-
-        self.client.close()
+def parse_host_string(host):
+    user, _, host = host.partition('@')
+    host, _, port = host.partition(':')
+    port = (port and int(port))
+    return user, host, port
 
 class Proxy(paramiko.ServerInterface):
     timeout = 10
     host_key = paramiko.RSAKey(filename='server-key')
 
-    def __init__(self, remote):
+    def __init__(self, socket, args):
         self.username = None
         self.queue = queue.Queue()
 
-        self.transport = paramiko.Transport(FakeSocket())
+        self.transport = paramiko.Transport(socket)
         self.transport.add_server_key(self.host_key)
         self.transport.start_server(server=self)
 
         try:
-            channel, command = self.queue.get(self.timeout)
+            client, command = self.queue.get(self.timeout)
         except queue.Empty:
             logging.error('Client passed no commands')
-            sys.exit(1)
-        Pipe(channel, remote).run(command)
-        self.transport.close()
+            self.transport.close()
+            return
+        except Exception as e:
+            self.transport.close()
+            raise e
+
+        self.remote = None
+        try:
+            self.remote = self.connect_to_remote(args.remote, args.remote_port, args.username or self.username)
+            remote = self.remote.get_transport().open_session()
+            remote.exec_command(command)
+
+            size = 1024
+            while True:
+                r, w, x = select.select([client, remote], [], [])
+
+                if remote in r:
+                    stdout = (remote.recv_ready() and remote.recv(size))
+                    if stdout:
+                        client.sendall(stdout)
+                    stderr = (remote.recv_stderr_ready() and remote.recv_stderr(size))
+                    if stderr:
+                        client.sendall_stderr(stderr)
+                    if not stdout and not stderr:
+                        logging.debug('Output streams closed')
+                        break
+
+                if client in r:
+                    content = client.recv(size)
+                    if not content:
+                        logging.debug('Input stream closed')
+                        break
+                    remote.sendall(content)
+
+            if remote.exit_status_ready():
+                status = remote.recv_exit_status()
+                client.send_exit_status(status)
+        finally:
+            client.close()
+            if self.remote:
+                self.remote.close()
+            self.transport.close()
+
+    @classmethod
+    def connect_to_remote(cls, host, port, username):
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        logging.info('Connecting to ssh host %s@%s:%s ...', username, host, port)
+        client.connect(host, port, username=username)
+        return client
 
     def check_channel_request(self, kind, chanid):
         if kind == 'session':
@@ -116,7 +98,7 @@ class Proxy(paramiko.ServerInterface):
     def check_auth_none(self, username):
         self.username = username
         return paramiko.AUTH_SUCCESSFUL
-    
+
     def get_allowed_auths(self, username):
         return 'none'
 
@@ -124,15 +106,43 @@ class Proxy(paramiko.ServerInterface):
         self.queue.put((channel, command))
         return True
 
+def run_server(host, port, args):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    logging.debug('bind()')
+    sock.bind((host, port))
+
+    logging.debug('listen()')
+    sock.listen(100)
+
+    threads = []
+    try:
+        while True:
+            # clean up closed connections
+            threads = [t for t in threads if not t.isAlive()]
+
+            logging.debug('accept()')
+            client, address = sock.accept()
+            logging.info('Got a connection!')
+
+            thread = threading.Thread(target=Proxy, args=(client, args))
+            thread.daemon = True
+            threads.append(thread)
+            thread.start()
+    except KeyboardInterrupt:
+        # stop server on ctrl+c
+        sock.close()
+        sys.exit(0)
+
 if __name__ == '__main__':
     SSH_PORT = 22
     parser = argparse.ArgumentParser(description='Forward all SSH requests to remote but authenticating as the proxy')
-    parser.add_argument('host', nargs='?', default='', help='Remote host')
-    parser.add_argument('port', nargs='?', default=SSH_PORT, type=int, help='Remote port (default {})'.format(SSH_PORT))
-    parser.add_argument('username', nargs='?', help='Username')
-    remote_client = make_client(args.host, args.host, args.username)
-    remote_channel = remote_client.get_transport().open_session()
+    parser.add_argument('remote', help='Remote host ([USER@]HOST:[PORT]). Default port is same as port argument.')
+    parser.add_argument('port', nargs='?', default=SSH_PORT, type=int, help='Port (default {})'.format(SSH_PORT))
+    parser.add_argument('host', nargs='?', default='', help='Host')
 
-    Proxy(remote_channel)
-    remote_client.close()
+    args = parser.parse_args()
+    args.username, args.remote, args.remote_port = parse_host_string(args.remote)
+    args.remote_port = (args.remote_port or args.port)
 
+    run_server(args.host, args.port, args)
