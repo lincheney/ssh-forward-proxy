e9c4fac48005b54f1d24dbc5702c70ae1205b02a
Cheney Lin <lincheney@gmail.com>
2015-04-25 18:39:09 +1000

    disable host checking in tests

--- tests/git_ssh_proxy.sh
+++ tests/git_ssh_proxy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=no -o ProxyCommand="ssh -p4000 localhost $PROXY_CMD" "$@"
+exec ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=no -o ProxyCommand="ssh -o StrictHostKeyChecking=no -p4000 localhost $PROXY_CMD" "$@"
