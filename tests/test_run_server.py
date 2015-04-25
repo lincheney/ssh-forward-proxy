13ff8daba869a8f915f5e83682cdc07e20fd5561
Cheney Lin <lincheney@gmail.com>
2015-04-25 21:23:10 +1000

    test that run_server closes socket on errors

--- tests/test_run_server.py
+++ tests/test_run_server.py
@@ -6,18 +6,15 @@ except ImportError:
 patch = mock.patch
 sentinel = mock.sentinel
 
+import os
+import signal
+
 from ssh_forward_proxy import run_server
 
 class RunServerTest(unittest.TestCase):
 
     class Error(Exception):
         pass
-
-    def setUp(self):
-        self.sys_exit_patch = patch('sys.exit')
-        self.sys_exit_patch.start()
-    def tearDown(self):
-        self.sys_exit_patch.stop()
 
     @patch('socket.socket.bind')
     def test_bind(self, bind):
@@ -26,7 +23,7 @@ class RunServerTest(unittest.TestCase):
         """
 
         # raise an error to stop the server going into the accept loop
-        with patch('socket.socket.accept', side_effect=self.Error() ):
+        with patch('socket.socket.accept', side_effect=self.Error):
             try:
                 run_server('host', 1234)
             except self.Error:
@@ -36,14 +33,14 @@ class RunServerTest(unittest.TestCase):
     @patch('socket.socket.bind')
     @patch('socket.socket.accept', return_value=(sentinel.socket, sentinel.address))
     @patch('threading.Thread')
-    def test_pserve_thread(self, Thread, accept, bind):
+    def test_server_thread(self, Thread, accept, bind):
         """
         the server should start a thread for the proxy on accept()
         """
 
         # raise an error to stop the server going into the accept loop
         thread = Thread.return_value
-        thread.start = mock.Mock(side_effect=self.Error() )
+        thread.start = mock.Mock(side_effect=self.Error)
 
         try:
             run_server('host', 1234, key='value', worker=sentinel.worker)
@@ -51,3 +48,30 @@ class RunServerTest(unittest.TestCase):
             pass
         Thread.assert_called_once_with(target=sentinel.worker, args=(sentinel.socket,), kwargs={'key': 'value'})
         thread.start.assert_called_once_with()
+
+    @patch('socket.socket.close')
+    @patch('socket.socket.bind')
+    def test_keyboard_interrupt(self, bind, close):
+        """
+        the server should start a thread for the proxy on accept()
+        """
+
+        with patch('socket.socket.accept', side_effect=self.send_sig_int):
+            run_server('host', 1234, key='value', worker=sentinel.worker)
+            close.assert_called_once_with()
+
+    def send_sig_int(self):
+        pid = os.getpid()
+        os.kill(pid, signal.SIGINT)
+
+    @patch('socket.socket.close')
+    @patch('socket.socket.bind')
+    def test_error(self, bind, close):
+        """
+        the server should start a thread for the proxy on accept()
+        """
+
+        with patch('socket.socket.accept', side_effect=self.Error):
+            with self.assertRaises(self.Error):
+                run_server('host', 1234, key='value', worker=sentinel.worker)
+            close.assert_called_once_with()
